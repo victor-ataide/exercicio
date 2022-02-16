7bd3be3b833149fc229646c5e51df0ea1362fc08
victor-ataide <89982093+victor-ataide@users.noreply.github.com>
2022-02-16 09:41:52 -0300

    Add files via upload

---
+++ 1004.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+
+int main (){
+
+    int a, b, prod;
+
+    printf("Qual o valor de a: ");
+    scanf("%d", &a);
+    printf("Qual o valor de b: ");
+    scanf("%d", &b);
+
+    prod = (a * b);
+
+    printf ("PROD = %d", prod);
+
+} 
